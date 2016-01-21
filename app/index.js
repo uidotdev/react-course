f1c296982a3f11c5968a5f6c9528eeec92239c83
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-01-20 23:14:07 -0700

    Add changes for Video3

--- app/index.js
+++ app/index.js
@@ -1,12 +1,38 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
+var Container = React.createClass({
   render: function () {
     return (
-      <div>Hello ReactJS Program!</div>
+      <Wrapper>
+        <Welcome name={'Tyler'}/>
+        <Hello />
+      </Wrapper>
     )
   }
 });
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+var Wrapper = React.createClass({
+  render: function () {
+    return (
+      <div style={{backgroundColor: 'pink'}}>
+        <h1> Header! </h1>
+        {this.props.children}
+      </div>
+    )
+  }
+});
+
+var Welcome = React.createClass({
+  render: function () {
+    return <div>Welcome {this.props.name}!</div>
+  }
+});
+
+var Hello = React.createClass({
+  render: function () {
+    return <div>Hello ReactJS Program!</div>
+  }
+})
+
+ReactDOM.render(<Container />, document.getElementById('app'));
