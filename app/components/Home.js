5cc07d6b03080ef16536ebf7cfb5d1d60eb6cd63
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-01-20 23:26:25 -0700

    Add code for video4

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Hello = React.createClass({
+  render: function () {
+    return (
+      <div> Hello from Home! </div>
+    )
+  }
+});
+
+module.exports = Hello;
--- app/index.js
+++ app/index.js
@@ -1,38 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var Container = React.createClass({
-  render: function () {
-    return (
-      <Wrapper>
-        <Welcome name={'Tyler'}/>
-        <Hello />
-      </Wrapper>
-    )
-  }
-});
-
-var Wrapper = React.createClass({
-  render: function () {
-    return (
-      <div style={{backgroundColor: 'pink'}}>
-        <h1> Header! </h1>
-        {this.props.children}
-      </div>
-    )
-  }
-});
-
-var Welcome = React.createClass({
-  render: function () {
-    return <div>Welcome {this.props.name}!</div>
-  }
-});
-
-var Hello = React.createClass({
-  render: function () {
-    return <div>Hello ReactJS Program!</div>
-  }
-})
-
-ReactDOM.render(<Container />, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
