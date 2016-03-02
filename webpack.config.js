32d09aa5e7b9c85ac425bf627e6350d4b4c1eb88
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-03-02 11:16:27 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"},
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
       { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
