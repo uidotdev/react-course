6bb0d9e5b45076fa187f1a4bf0816b27ef9055f8
Luis Porras <lporras16@gmail.com>
2016-06-29 19:58:35 -0300

    adding changes for Avatar, ProfileLink, ProfileName, ProfilePic example

--- app/index.js
+++ app/index.js
@@ -1,38 +1,55 @@
+var USER_DATA = {
+  name: 'Tyler McGinnis',
+  username: 'tylermcginnis',
+  image: 'https://avatars0.githubusercontent.com/u/2933430?v=3$s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Container = React.createClass({
+/*
+  Focused
+  Independent
+  Reusable
+  Small
+  Testable
+*/
+
+var ProfilePic = React.createClass({
   render: function () {
-    return (
-      <Wrapper>
-        <Welcome name={'Tyler'}/>
-        <Hello />
-      </Wrapper>
-    )
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
   }
 });
 
-var Wrapper = React.createClass({
+var ProfileLink = React.createClass({
   render: function () {
     return (
-      <div style={{backgroundColor: 'pink'}}>
-        <h1> Header! </h1>
-        {this.props.children}
+      <div>
+        <a href={'https://www.github.com/' +  this.props.username}>
+          {this.props.username}
+        </a>
       </div>
-    )
+    );
   }
 });
 
-var Welcome = React.createClass({
-  render: function () {
-    return <div>Welcome {this.props.name}!</div>
+var ProfileName = React.createClass({
+  render: function (){
+    return <div>{this.props.name}</div>
   }
 });
 
-var Hello = React.createClass({
+var Avatar = React.createClass({
   render: function () {
-    return <div>Hello ReactJS Program!</div>
+    return(
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
   }
-})
+});
 
-ReactDOM.render(<Container />, document.getElementById('app'));
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
