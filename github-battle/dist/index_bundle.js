90b05b01fa5bfe54c595600f6ace0519e0394fd6
Lane Milos <lanemilos@hotmail.com>
2017-01-14 21:45:22 -0700

    Successfully webpack code

---
+++ github-battle/dist/index_bundle.js
@@ -0,0 +1,1 @@
+!function(r){function t(e){if(o[e])return o[e].exports;var n=o[e]={exports:{},id:e,loaded:!1};return r[e].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var o={};return t.m=r,t.c=o,t.p="",t(0)}([function(r,t,o){r.exports=o(1)},function(r,t){a}]);
