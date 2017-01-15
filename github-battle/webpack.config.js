dc68bdc1c088f90a0bf33a6e22873ecefe71eb11
Lane Milos <lanemilos@hotmail.com>
2017-01-14 21:37:43 -0700

    Added webpack config settings for HTMLWebpackPlugin and HTMLWebpackPluginConifg

--- github-battle/webpack.config.js
+++ github-battle/webpack.config.js
@@ -1,3 +1,10 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
   entry: [
     './app/index.js'
@@ -10,5 +17,6 @@ module.exports = {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
