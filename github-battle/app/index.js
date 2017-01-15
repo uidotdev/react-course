3770a86509f9044edcd8f515acf004c715158e1e
Lane Milos <lanemilos@hotmail.com>
2017-01-14 22:04:40 -0700

    Set up first React Component

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,2 +1,15 @@
-var app = document.getElementById('app');
-app.innerHTML = "Hello";
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div> Hello ReactJS Program </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById("app")
+);
