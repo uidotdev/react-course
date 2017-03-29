7fd17476ba3327e0e9d179142031d78896909a1f
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-03-29 16:37:31 -0600

    Changes for 'popular' branch

---
+++ app/components/Popular.js
@@ -0,0 +1,41 @@
+var React = require('react');
+
+class Popular extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      selectedLanguage: 'All',
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(lang) {
+    this.setState(function () {
+      return {
+        selectedLanguage: lang,
+      }
+    });
+  }
+  render() {
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+      <div>
+        <ul className='languages'>
+          {languages.map(function (lang) {
+            return (
+              <li
+                style={lang === this.state.selectedLanguage ? {color: '#d0021b'} : null}
+                onClick={this.updateLanguage.bind(null, lang)}
+                key={lang}>
+                  {lang}
+              </li>
+            )
+          }, this)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+module.exports = Popular;
