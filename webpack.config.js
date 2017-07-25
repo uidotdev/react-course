1a83fc78c9e2cd1be218fcf96242ff5ea114fcd2
Felix Volny <felix@volny.co>
2017-07-25 14:26:49 +0700

    babel-preset-stage-2 and babel-polyfill added

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 
 var config = {
-  entry: './app/index.js',
+  entry: [
+    'babel-polyfill',
+    './app/index.js'
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
