60429670785686312ae563de908fd1fbac39fabb
Tyler McGinnis <tylermcginnis33@gmail.com>
2019-03-12 17:31:13 -0600

    9. Battle Instructions

---
+++ app/components/Battle.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+
+function Instructions () {
+  return (
+    <div className='instructions-container'>
+      <h1 className='center-text header-lg'>
+        Instructions
+      </h1>
+      <ol className='container-sm grid center-text battle-instructions'>
+        <li>
+          <h3 className='header-sm'>Enter two Github users</h3>
+          <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
+        </li>
+        <li>
+          <h3 className='header-sm'>Battle</h3>
+          <FaFighterJet className='bg-light' color='#727272' size={140} />
+        </li>
+        <li>
+          <h3 className='header-sm'>See the winners</h3>
+          <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
+        </li>
+      </ol>
+    </div>
+  )
+}
+
+export default class Battle extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Instructions />
+      </React.Fragment>
+    )
+  }
+}
--- app/index.js
+++ app/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
+import Battle from './components/Battle'
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <Popular />
+        <Battle />
       </div>
     )
   }
