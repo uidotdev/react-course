0f62bef85bded0fb98e0c6607e7acba8b7220296
Tyler McGinnis <tylermcginnis33@gmail.com>
2019-03-11 22:23:22 -0600

    2. Popular Navbar

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div className='container'>
+        <Popular />
       </div>
     )
   }
