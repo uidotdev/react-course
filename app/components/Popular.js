bdc22a9d561f7121b3ab3f4d717f35ec47ae6b76
Tyler McGinnis <tylermcginnis33@gmail.com>
2019-03-13 11:27:36 -0600

    16. Results UI

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -35,7 +35,7 @@ function ReposGrid ({ repos }) {
         const { login, avatar_url } = owner
 
         return (
-          <li key={html_url} className='repo bg-light'>
+          <li key={html_url} className='card bg-light'>
             <h4 className='header-lg center-text'>
               #{index + 1}
             </h4>
@@ -136,7 +136,7 @@ export default class Popular extends React.Component {
 
         {this.isLoading() && <p>LOADING</p>}
 
-        {error && <p>{error}</p>}
+        {error && <p className='center-text error'>{error}</p>}
 
         {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
       </React.Fragment>
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,20 +1,139 @@
 import React from 'react'
 import { battle } from '../utils/api'
+import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
 
 export default class Results extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
   componentDidMount () {
     const { playerOne, playerTwo } = this.props
 
     battle([ playerOne, playerTwo ])
       .then((players) => {
-        console.log('data: ', players)
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          error: null,
+          loading: false
+        })
+      }).catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false
+        })
       })
   }
   render() {
+    const { winner, loser, error, loading } = this.state
+
+    if (loading === true) {
+      return <p>LOADING</p>
+    }
+
+    if (error) {
+      return (
+        <p className='center-text error'>{error}</p>
+      )
+    }
+
     return (
-      <div>
-        Results
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+      <div className='grid space-around container-sm'>
+        <div className='card bg-light'>
+          <h4 className='header-lg center-text'>
+            {winner.score === loser.score ? 'Tie' : 'Winner'}
+          </h4>
+          <img
+            className='avatar'
+            src={winner.profile.avatar_url}
+            alt={`Avatar for ${winner.profile.login}`}
+          />
+          <h4 className='center-text'>
+            Score: {winner.score.toLocaleString()}
+          </h4>
+          <h2 className='center-text'>
+            <a className='link' href={winner.profile.html_url}>
+              {winner.profile.login}
+            </a>
+          </h2>
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {winner.profile.name}
+            </li>
+            {winner.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {winner.profile.location}
+              </li>
+            )}
+            {winner.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {winner.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {winner.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {winner.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </div>
+        <div className='card bg-light'>
+          <h4 className='header-lg center-text'>
+            {winner.score === loser.score ? 'Tie' : 'Loser'}
+          </h4>
+          <img
+            className='avatar'
+            src={loser.profile.avatar_url}
+            alt={`Avatar for ${loser.profile.login}`}
+          />
+          <h4 className='center-text'>
+            Score: {loser.score.toLocaleString()}
+          </h4>
+          <h2 className='center-text'>
+            <a className='link' href={loser.profile.html_url}>
+              {loser.profile.login}
+            </a>
+          </h2>
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {loser.profile.name}
+            </li>
+            {loser.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {loser.profile.location}
+              </li>
+            )}
+            {loser.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {loser.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {loser.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {loser.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
