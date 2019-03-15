b6a24e1349d8dec1efb73ec104937d932f4f3b08
Tyler McGinnis <tylermcginnis33@gmail.com>
2019-03-15 15:44:01 -0600

    28. Navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import { ThemeConsumer } from '../contexts/theme'
+import { NavLink } from 'react-router-dom'
+
+const activeStyle = {
+  color: 'rgb(187, 46, 31)'
+}
 
 export default function Nav () {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className='row space-between'>
+          <ul className='row nav'>
+            <li>
+              <NavLink
+                to='/'
+                exact
+                activeStyle={activeStyle}
+                className='nav-link'>
+                  Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/battle'
+                activeStyle={activeStyle}
+                className='nav-link'>
+                  Battle
+              </NavLink>
+            </li>
+          </ul>
           <button
             style={{fontSize: 30}}
             className='btn-clear'
