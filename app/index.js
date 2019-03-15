2251dbd5fef1c9ea011af65958a1d353179b4fd7
Tyler McGinnis <tylermcginnis33@gmail.com>
2019-03-15 14:31:21 -0600

    25. Toggle Theme

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
 import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
 
 class App extends React.Component {
   constructor(props) {
@@ -21,8 +22,12 @@ class App extends React.Component {
   render() {
     return (
       <ThemeProvider value={this.state}>
-        <div className='container'>
-          <Battle />
+        <div className={this.state.theme}>
+          <div className='container'>
+            <Nav />
+
+            <Battle />
+          </div>
         </div>
       </ThemeProvider>
     )
