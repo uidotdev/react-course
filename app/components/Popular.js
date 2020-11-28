9ac966dada0fd661ed0ece48668ceecdcde5e43c
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-11-28 16:26:32 -0700

    3. Navbar State

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,20 @@
 import * as React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
@@ -8,7 +22,10 @@ export default class Popular extends React.Component {
       <ul className='flex-center'>
         {languages.map((language) => (
           <li key={language}>
-            <button className='btn-clear nav-link'>
+            <button
+              className='btn-clear nav-link'
+              style={language === this.state.selectedLanguage ? { color: 'rgb(187, 46, 31)' } : null}
+              onClick={() => this.updateLanguage(language)}>
               {language}
             </button>
           </li>
