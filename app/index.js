6438d1abc953c052efc464f80892050f575e9d65
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-11-28 16:53:54 -0700

    9. Battle Instructions

--- app/index.js
+++ app/index.js
@@ -2,12 +2,13 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
+import Battle from './components/Battle'
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <Popular />
+        <Battle />
       </div>
     )
   }
