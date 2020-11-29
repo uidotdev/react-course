c47758b572fef8e91a5c44018b11a51be0b0a31a
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-11-28 18:34:52 -0700

    19. Reset Players

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -140,7 +140,17 @@ export default class Battle extends React.Component {
     const { playerOne, playerTwo, battle } = this.state
 
     if (battle === true) {
-      return <Results playerOne={playerOne} playerTwo={playerTwo} />
+      return (
+        <Results
+          playerOne={playerOne}
+          playerTwo={playerTwo}
+          onReset={() => this.setState({
+            playerOne: null,
+            playerTwo: null,
+            battle: false
+          })}
+        />
+      )
     }
 
     return (
