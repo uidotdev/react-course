aa7d25f62b47ca85d4a49d96429510233a943388
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-11-28 18:18:35 -0700

    15. Results Data

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,19 +2,39 @@ import * as React from 'react'
 import { battle } from '../utils/api'
 
 export default class Results extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
   componentDidMount () {
     const { playerOne, playerTwo } = this.props
 
     battle([ playerOne, playerTwo ])
       .then((players) => {
-        console.log('data: ', players)
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          error: null,
+          loading: false
+        })
+      }).catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false
+        })
       })
   }
   render() {
     return (
       <div>
         Results
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     )
   }
