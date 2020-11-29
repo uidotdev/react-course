91bd11bf331cf602d18310320bb560110b93bb05
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-11-28 19:08:44 -0700

    34. Netflify Client Side Routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
@@ -18,7 +19,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
-    })
+    }),
+    new CopyPlugin({ patterns: [{ from : '_redirects' }] })
   ],
   devServer: {
     historyApiFallback: true
