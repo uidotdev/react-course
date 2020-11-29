410934e852707ea43334877956bc13e698f1c79e
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-11-28 18:41:03 -0700

    22. withHover

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
+import withHover from './withHover'
 
 const styles = {
   container: {
@@ -23,43 +24,18 @@ const styles = {
   }
 }
 
-export default class Tooltip extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      hovering: false,
-    }
-
-    this.mouseOver = this.mouseOver.bind(this)
-    this.mouseOut = this.mouseOut.bind(this)
-  }
-  mouseOver() {
-    this.setState({
-      hovering: true
-    })
-  }
-  mouseOut() {
-    this.setState({
-      hovering: false
-    })
-  }
-  render() {
-    const { text, children } = this.props
-    const { hovering } = this.state
-
-    return (
-      <div
-        onMouseOver={this.mouseOver}
-        onMouseOut={this.mouseOut}
-        style={styles.container}>
-          {hovering === true && <div style={styles.tooltip}>{text}</div>}
-          {children}
-      </div>
-    )
-  }
+function Tooltip ({ text, children, hovering }) {
+  return (
+    <div style={styles.container}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  )
 }
 
 Tooltip.propTypes = {
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  hovering: PropTypes.bool.isRequired,
 }
+
+export default withHover(Tooltip)
