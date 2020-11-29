e314c0af7a5725b3e583e2a7a926e94e4411cee7
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-11-28 18:51:28 -0700

    27. Adding Routes

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import Popular from './components/Popular'
 import Battle from './components/Battle'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
   constructor(props) {
@@ -21,15 +22,18 @@ class App extends React.Component {
   }
   render() {
     return (
-      <ThemeProvider value={this.state}>
-        <div className={this.state.theme}>
-          <div className='container'>
-            <Nav />
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className='container'>
+              <Nav />
 
-            <Popular />
+              <Route exact path='/' component={Popular} />
+              <Route path='/battle' component={Battle} />
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </Router>
     )
   }
 }
