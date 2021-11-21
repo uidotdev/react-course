49713f99598e82558e19531a2eefc61093795c7d
Steffan Harris <contact@steffan.lol>
2021-11-21 11:47:08 +0000

    Toggle theme

---
+++ app/components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { ThemeConsumer } from "../contexts/theme";
+
+export default function Nav() {
+  return (
+    <ThemeConsumer>
+      {({ theme, toggleTheme }) => (
+        <nav className="row space-between">
+          <button
+            style={{ fontSize: 30 }}
+            className="btn-clear"
+            onClick={toggleTheme}
+          >
+            {theme === "light" ? "🔦" : "💡"}
+          </button>
+        </nav>
+      )}
+    </ThemeConsumer>
+  );
+}
--- app/index.js
+++ app/index.js
@@ -3,13 +3,33 @@ import * as ReactDOM from "react-dom";
 import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
+import { ThemeProvider } from "./contexts/theme";
+import Nav from "./components/Nav";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === "light" ? "dark" : "light",
+        }));
+      },
+    };
+  }
+
   render() {
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            <Battle />
+          </div>
+        </div>
+      </ThemeProvider>
     );
   }
 }
