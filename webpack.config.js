c1930e802a91bde89379e07b4d243542e8fd326a
Steffan Harris <contact@steffan.lol>
2021-11-30 00:18:28 +0000

    Production build

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
   },
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: "app/index.html",
