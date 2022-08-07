70f5979bae77f93e5839efff57e6449b3e5e3992
Tyler McGinnis <tylermcginnis33@gmail.com>
2022-08-06 18:28:17 -0600

    16-hover

--- app/components/Tooltip.jsx
+++ app/components/Tooltip.jsx
@@ -1,28 +1,28 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import withHover from "./withHover";
+import Hover from "./Hover";
 
 const container = {
   position: "relative",
   display: "flex",
 };
 
-class Tooltip extends React.Component {
-  render() {
-    const { children, element, hovering } = this.props;
-
-    return (
-      <div style={container}>
-        {hovering === true && element}
-        {children}
-      </div>
-    );
-  }
+export default function Tooltip({ children, element }) {
+  return (
+    <Hover>
+      {(hovering) => {
+        return (
+          <div style={container}>
+            {hovering === true && element}
+            {children}
+          </div>
+        );
+      }}
+    </Hover>
+  );
 }
 
 Tooltip.propTypes = {
   children: PropTypes.node.isRequired,
   element: PropTypes.node.isRequired,
 };
-
-export default withHover(Tooltip);
